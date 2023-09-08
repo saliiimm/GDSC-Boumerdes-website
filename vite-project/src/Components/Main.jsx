b45279669bc0bf18706ebdbebb5826d948851f51
saliiimm <salimghalem40@gmmail.com>
2023-09-08 23:11:22 +0100

    70%

--- vite-project/src/Components/Main.jsx
+++ vite-project/src/Components/Main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import mainImage from "../assets/group.svg";
+import "../App.css";
 
 const Main = () => {
   return (
-    <div className="pt-[80px] grid grid-cols-2 mb-100">
+    <div className="pt-[80px] grid md:grid-cols-2 mb-5 sm:grid-cols-0">
       <div className="mt-20 pl-20">
         <h2 className="text-7xl font-bold text-green-600 mb-10">Main Title</h2>
         <p className="mt-19 mb-10 text-4xl text-gray-500 font-sans max-w-xl">
@@ -11,11 +12,11 @@ const Main = () => {
           ullam. Ducimus tempore natus, iure dignissimos sunt amet voluptatem
           voluptatum perspiciatis eaque omnis vel
         </p>
-        <button className="mt-19 bg-green-600 text-white h-12 w-40 rounded-md">
+        <button className="mt-22 bg-green-600 text-white h-14 w-48 text-xl font-bold rounded-xl">
           Click Here
         </button>
       </div>
-      <div className="">
+      <div className="xs:hidden md:flex">
         <img src={mainImage} alt="image group" className="mt-20" />
       </div>
     </div>
--- vite-project/src/Components/Team.jsx
+++ vite-project/src/Components/Team.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <div className="flex items-center justify-center flex-col mt-20">
+      <h2 className="text-6xl font-bold text-red-500 mb-10">Our team</h2>
+      <p className=" mb-10 text-2xl text-gray-500 font-sans text-center max-w-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio numquam
+        quam ex consectetur non, nam iure tempora vel blanditiis possimus atque
+        illo
+      </p>
+    </div>
+  );
+};
 
-export default Team
+export default Team;

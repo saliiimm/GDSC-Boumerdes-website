5819eefbf65f6bcc60bb33714b356b6d8e33437d
saliiimm <salimghalem40@gmmail.com>
2023-09-09 23:24:31 +0100

    db + package json + yarn + app.css + main + team

--- vite-project/src/Components/Team.jsx
+++ vite-project/src/Components/Team.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "../App.css";
+import { Autoplay } from "swiper/modules";
 
 const Team = () => {
   return (
@@ -9,6 +13,27 @@ const Team = () => {
         quam ex consectetur non, nam iure tempora vel blanditiis possimus atque
         illo
       </p>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={false}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        slidesPerView={3}
+        navigation={true}
+        modules={[Autoplay]}
+        className="mySwiper">
+        <SwiperSlide style={{ width: "300px" }}>Slide 1</SwiperSlide>
+        <SwiperSlide style={{ width: "300px" }}>Slide 2</SwiperSlide>
+        <SwiperSlide style={{ width: "300px" }}>Slide 3</SwiperSlide>
+        <SwiperSlide style={{ width: "300px" }}>Slide 4</SwiperSlide>
+        <SwiperSlide style={{ width: "300px" }}>Slide 5</SwiperSlide>
+        <SwiperSlide style={{ width: "300px" }}>Slide 6</SwiperSlide>
+        <SwiperSlide style={{ width: "300px" }}>Slide 7</SwiperSlide>
+        <SwiperSlide style={{ width: "300px" }}>Slide 8</SwiperSlide>
+        <SwiperSlide style={{ width: "300px" }}>Slide 9</SwiperSlide>
+      </Swiper>
     </div>
   );
 };

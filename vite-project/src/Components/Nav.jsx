4eb9f8571d3e1072eb9b12ce78fb0cb3df974f27
sido200 <93400204+sido200@users.noreply.github.com>
2023-09-09 23:09:33 +0100

    sidali

--- vite-project/src/Components/Nav.jsx
+++ vite-project/src/Components/Nav.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import image from '../assets/logo.png';
 import { HiMenu } from 'react-icons/hi';
+import '../App.css';
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [navWidth, setNavWidth] = useState('nav');
   const navRef = useRef(null);
   const burgerRef = useRef(null);
 
@@ -13,9 +15,12 @@ const Nav = () => {
         navRef.current &&
         burgerRef.current &&
         !navRef.current.contains(event.target) &&
-        !burgerRef.current.contains(event.target)
+        !burgerRef.current.contains(event.target) &&
+        isOpen // Vérifiez si le menu est ouvert avant de fermer l'overlay
       ) {
         setIsOpen(false);
+        document.body.classList.remove('menu-open', 'overlay');
+        setNavWidth('nav');
       }
     };
 
@@ -24,14 +29,21 @@ const Nav = () => {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, []);
+  }, [isOpen]);
 
   const handleBar = () => {
     setIsOpen(!isOpen);
+    if (isOpen) {
+      setNavWidth('nav');
+      document.body.classList.remove('menu-open', 'overlay');
+    } else {
+      setNavWidth('active');
+      document.body.classList.add('menu-open', 'overlay');
+    }
   };
 
   return (
-    <nav className="flex px-4 items-center absolute w-screen h-20 justify-between md:px-8 shadow-md overflow-visible z-10">
+    <nav className={`flex px-4 items-center absolute w-[100%] h-20 justify-between md:px-8 shadow-md z-10`}>
       <div>
         <img
           className="w-40 h-5 md:w-52 md:h-6"
@@ -53,9 +65,7 @@ const Nav = () => {
         />
       </div>
       <div
-        className={`menu-burger w-72 h-[89.3vh] absolute top-[80px] duration-200 right-0 ${
-          isOpen ? 'transform translate-x-0' : 'transform translate-x-full'
-        }`}
+        className={`menu-burger  md:hidden w-72 h-[89.3vh] absolute top-[80px] duration-200 right-0 ${navWidth}`}
         ref={navRef}
       >
         <ul className="bg-white flex gap-10 flex-col h-full items-center pt-7 shadow-lg">
--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Nav from '../Components/Nav'
 import Footer from '../Components/Footer'
+import Main from '../Components/Main'
+
 
 const Home = () => {
   return (
     <div>
         <Nav/>
+        <Main/>
         <Footer/>
     </div>
   )

868d9411e8c2173398c9d115519de8973bd64029
saliiimm <salimghalem40@gmmail.com>
2023-09-10 01:08:21 +0100

    our team

--- vite-project/src/Components/Team.jsx
+++ vite-project/src/Components/Team.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "../App.css";
-import { Autoplay } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
+import { useSwiper } from "swiper/react";
+import rightBtn from "../assets/rightbutton.svg";
+import leftBtn from "../assets/leftbutton.svg";
 
 const Team = () => {
+  const [membres, setMembres] = useState([]);
+  const swiper = useSwiper();
+  useEffect(() => {
+    axios
+      .get("../../db.json")
+      .then((response) => {
+        const data = response.data;
+        setMembres(data.membres);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="flex items-center justify-center flex-col mt-20">
       <h2 className="text-6xl font-bold text-red-500 mb-10">Our team</h2>
@@ -13,27 +30,50 @@ const Team = () => {
         quam ex consectetur non, nam iure tempora vel blanditiis possimus atque
         illo
       </p>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={false}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        slidesPerView={3}
-        navigation={true}
-        modules={[Autoplay]}
-        className="mySwiper">
-        <SwiperSlide style={{ width: "300px" }}>Slide 1</SwiperSlide>
-        <SwiperSlide style={{ width: "300px" }}>Slide 2</SwiperSlide>
-        <SwiperSlide style={{ width: "300px" }}>Slide 3</SwiperSlide>
-        <SwiperSlide style={{ width: "300px" }}>Slide 4</SwiperSlide>
-        <SwiperSlide style={{ width: "300px" }}>Slide 5</SwiperSlide>
-        <SwiperSlide style={{ width: "300px" }}>Slide 6</SwiperSlide>
-        <SwiperSlide style={{ width: "300px" }}>Slide 7</SwiperSlide>
-        <SwiperSlide style={{ width: "300px" }}>Slide 8</SwiperSlide>
-        <SwiperSlide style={{ width: "300px" }}>Slide 9</SwiperSlide>
-      </Swiper>
+      <div className="w-full flex items-center justify-evenly">
+        <img
+          src={leftBtn}
+          className="swiper-button-prev cursor-pointer ml-auto "
+          onClick={() => swiper.slidePrev()}
+        />
+        <Swiper
+          spaceBetween={0}
+          centeredSlides={false}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          slidesPerView={3}
+          navigation={{
+            prevEl: ".swiper-button-prev",
+            nextEl: ".swiper-button-next",
+          }}
+          modules={[Autoplay, Navigation]}
+          className="mySwiper">
+          {membres.map((membre) => (
+            <SwiperSlide key={membre.id}>
+              <div className="flex items-center justify-center flex-col w-60 h-72 shadow-md bg-white rounded-lg">
+                <img
+                  src={membre.img}
+                  alt="Profile picture"
+                  className="mb-10 w-24 h-24  rounded-full"
+                />
+                <h4 className="text-xl font-bold text-gray-800 mb-3">
+                  {membre.fullName}
+                </h4>
+                <h5 className=" text-gray-500 font-sans text-center font-light">
+                  {membre.role}
+                </h5>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <img
+          src={rightBtn}
+          className="swiper-button-next cursor-pointer mr-auto"
+          onClick={() => swiper.slideNext()}
+        />
+      </div>
     </div>
   );
 };
--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import Nav from '../Components/Nav'
-import FindUs from '../Components/FindUs'
-import Sponsor from '../Components/Sponsor'
-import Contact from '../Components/Contact'
-
-import Footer from '../Components/Footer'
-import Main from '../Components/Main'
-
-
+import React from "react";
+import Nav from "../Components/Nav";
+import FindUs from "../Components/FindUs";
+import Sponsor from "../Components/Sponsor";
+import Contact from "../Components/Contact";
+import Footer from "../Components/Footer";
+import Main from "../Components/Main";
+import Team from "../Components/Team";
+import Acitivities from "../Components/Acitivities";
+import Events from "../Components/Events";
 const Home = () => {
   return (
-    <div className='bg-gradient-to-tr from-orange-50 to-cyan-100 via-white  overflow-hidden'>
-        <Nav/>
-        <Sponsor/>
-        <FindUs/>
-        <Contact/>
-        <Footer/>
+    <div className="bg-gradient-to-tr from-orange-50 to-cyan-100 via-white  overflow-hidden">
+      <Nav />
+      <Main />
+      <Team />
+      <Acitivities />
+      <Events />
+      <Sponsor />
+      <FindUs />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

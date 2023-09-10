2adc63962f63fae9a4eb09492db33df0a2762524
Medjber Soundous <101836665+medjbersoundous@users.noreply.github.com>
2023-09-10 14:38:14 +0100

    kmlllttttt

--- vite-project/src/Components/Contact.jsx
+++ vite-project/src/Components/Contact.jsx
@@ -11,15 +11,16 @@ const onSubmit = async (values, actions) => {
 
 const Contact = () => {
   const style ={
-    input: 'w-[472px] md:w-[472px] p-[0.8rem]   m-2 text-[1rem] text-[#00000080] shadow-lg shadow-gray-500/50 rounded-[15px] bg-white outline-none border-2  focus:border-blue-500 placeholder-[#00000080] ',
+    input: 'w-[302px] lg:w-[472px] med:w-[472px]  p-[0.8rem]   m-2 text-[1rem] text-[#00000080] shadow-lg shadow-gray-500/50 rounded-[15px] bg-white outline-none border-2  focus:border-blue-500 placeholder-[#00000080] ',
     para: 'ml-5  mt-[-10px] text-[#fc8181]',
     message: 'h-[243px]  ',
-    button:"bg-[#FBBC04] md:w-[472px] w-[472px] h-[60px] ml-2 mt-2 rounded-[15px] uppercase text-white font-bold text-[18px] opacity-35",
+    button:"bg-[#FBBC04] lg:w-[472px] med:w-[472px] w-[302px] h-[60px] ml-2 mt-2 rounded-[15px] uppercase text-white font-bold text-[18px] opacity-35",
     input_error: 'border-[#fc8181]',
-    contact: 'text-[46px] text-[#FBBC04] font-bold mb-20 capitalize text-center',
-    form:" flex flex-col md:mr-4",
-    welcomepara:"w-[400px]  mb-[50px] medi:w-[300px] font-thin",
-    all:'flex flex-col-reverse   justify-center items-center md:flex-row md:justify-around   lg:w-[1200px] md:ml-[5%] '
+    contact: ' text-[#FBBC04]  capitalize font-semibold text-[30px] md:text-[58px] text-center  py-[5%]',
+    form:" flex items-center justify-center flex-col md:mr-4",
+    welcomepara:"w-[300px] md:text-[15px] md:ml-[-5%]  md:text-left text-center lg:text-[16px] lg:text-left xl:w-[450px]   mb-[50px] ",
+    pic:"xl:w-[340px]  md:w-[260px]  w-[200px] ml-[40px] medi:w-[250px]",
+    all:'flex flex-col-reverse   justify-center items-center md:flex-row md:justify-around   xl:w-[1200px] md:ml-[5%] '
   }
 
   const handleSubmite = () => {
@@ -108,9 +109,9 @@ const Contact = () => {
       </button>
      </form>
      <div >
-      <p className={style.welcomepara}>We value your input and welcome your questions, ideas, or messages. Our team is here to assist with any inquiries or concerns you may have. Don't hesitate to reach out via our contact form for prompt assistance and communication</p>
+      <p className={style.welcomepara}>We value your input and welcome your questions, ideas, or messages. Our team is here to assist with any inquiries or concerns you may have. Don't hesitate to reach out via our contact form for prompt assistance and communication.</p>
      
-      <img src={gdscboum} alt="" className="w-[320px] ml-[40px] medi:w-[250px] " />
+      <img src={gdscboum} alt="" className={style.pic} />
       
      </div>
      </div>

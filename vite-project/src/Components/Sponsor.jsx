6bbb542ad6a7b9c3b452e7d177fee3d10a38fab6
Medjber Soundous <101836665+medjbersoundous@users.noreply.github.com>
2023-09-12 12:23:41 +0100

    last modif

--- vite-project/src/Components/Sponsor.jsx
+++ vite-project/src/Components/Sponsor.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 const style = {
-  logo : "md:w-[600px]  w-[250px] mt-[40px]"
+  logo : "md:w-[600px]  w-[250px] mt-[70px]"
 }
 const Sponsor = () => {
   return (
     <>
-      <div className='text-center pt-[20%] '>
-        <h1 className='text-[#EA4335] font-bold mt-10 text-[30px]  md:text-[60px]'>Sponsors</h1>
+      <div className='text-center pt-[5%] '>
+        <h1 className='text-[#EA4335] font-bold  text-[30px]  md:text-[60px]'>Sponsors</h1>
         <div className='flex  justify-center  items-center'>
         <img className={style.logo} src="https://res.cloudinary.com/startup-grind/image/upload/dpr_2.0,fl_sanitize/v1/gcs/platform-data-dsc/contentbuilder/logo_dark_horizontal_097s7oa.svg"  alt="Google Developer Student Clubs logo"/>
         </div>

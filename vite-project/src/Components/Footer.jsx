dbdafcc860888fa243a9d5496fa999525e757ef2
sido200 <93400204+sido200@users.noreply.github.com>
2023-10-15 18:47:27 +0100

    sidali

--- vite-project/src/Components/Footer.jsx
+++ vite-project/src/Components/Footer.jsx
@@ -8,14 +8,14 @@ import '../App.css'
 const Footer = () => {
   return (
     <div className='flex flex-col bottom-0 '>
-      <div className='flex justify-around px-3 py-20 md:p-20  bg-blue  w-screen h-[300px]  ' >
+      <div className='flex justify-around px-3 py-20 md:p-20  bg-blue  w-screen h-[250px] md:h-[300px]  ' >
         <div className="logo-desc w-[200px] md:w-auto">
         <img
           className="w-[130px] h-5 md:w-[300px] md:h-9"
           src={image}
           alt="logo"
         />
-        <p className='text-white font-montserrat text-[13px] md:text-[16px]  font-light w-[180px] md:w-[250px] h-[87px] py-5'>
+        <p className='text-white font-montserrat text-[10px] md:text-[16px]  font-light w-[180px] md:w-[300px] xl:w-[350px] h-[87px] py-5'>
           Explore the world of technology and innovation with GDSC UMBB. <br /> 
           Join us in our mission to inspire and educate, making a difference in the tech community one step at a time.
            </p>
@@ -33,7 +33,7 @@ const Footer = () => {
           <p className="text-white  text-[15px]  md:text-[20px] cursor-pointer capitalize  pb-2 "><Link to='https://www.linkedin.com/company/gdscumbb/mycompany/' className='flex items-center'><AiFillLinkedin className='mr-1  text-[28px]'/>  <span className='hidden md:block'>linkedin</span></Link> </p>
         </div>
       </div>
-      <div className="bg-blue h-[54px] w-screen border-t bg-blue-2 bleu flex text-white text-[15px] justify-center items-center">
+      <div className="bg-blue h-[54px] w-screen border-t bg-blue-2 bleu flex text-white text-[13px] md:text-[15px] justify-center items-center">
       Copyright © 2023 GDSC UMBB. All Rights Reserved.
       </div>
     </div>
